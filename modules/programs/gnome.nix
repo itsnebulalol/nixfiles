c6ada425a2d10aa3293425715f6d0349fe701b42
Nebula <me@itsnebula.net>
2024-02-09 18:20:58 -0500

    chore(gnome): organize os.*

--- modules/programs/gnome.nix
+++ modules/programs/gnome.nix
@@ -8,16 +8,36 @@
 
   config = lib.mkMerge [
     (lib.mkIf config.programs.gnome.enable {
-      os.services.xserver = {
-        enable = true;
-        desktopManager.gnome.enable = true;
-        displayManager.gdm.enable = true;
-      };
-      os.services.gnome.games.enable = false;
+      os = {
+        services = {
+          xserver = {
+            enable = true;
+            desktopManager.gnome.enable = true;
+            displayManager.gdm.enable = true;
+          };
+          gnome.games.enable = false;
 
-      # Systray
-      os.environment.systemPackages = [ pkgs.gnomeExtensions.appindicator ];
-      os.services.udev.packages = [ pkgs.gnome.gnome-settings-daemon ];
+          udev.packages = [ pkgs.gnome.gnome-settings-daemon ];
+        };
+
+        environment.systemPackages = [ pkgs.gnomeExtensions.appindicator ];
+
+        # Dynamic triple buffering
+        nixpkgs.overlays = [
+          (final: prev: {
+            gnome = prev.gnome.overrideScope' (gnomeFinal: gnomePrev: {
+              mutter = gnomePrev.mutter.overrideAttrs ( old: {
+                src = pkgs.fetchgit {
+                  url = "https://gitlab.gnome.org/vanvugt/mutter.git";
+                  # GNOME 45: triple-buffering-v4-45
+                  rev = "0b896518b2028d9c4d6ea44806d093fd33793689";
+                  sha256 = "sha256-mzNy5GPlB2qkI2KEAErJQzO//uo8yO0kPQUwvGDwR4w=";
+                };
+              } );
+            });
+          })
+        ];
+      };
 
       # Dconf
       hm.users.nebula = {
@@ -34,22 +54,6 @@
           ];
         };
       };
-
-      # Dynamic triple buffering
-      nixpkgs.overlays = [
-        (final: prev: {
-          gnome = prev.gnome.overrideScope' (gnomeFinal: gnomePrev: {
-            mutter = gnomePrev.mutter.overrideAttrs ( old: {
-              src = pkgs.fetchgit {
-                url = "https://gitlab.gnome.org/vanvugt/mutter.git";
-                # GNOME 45: triple-buffering-v4-45
-                rev = "0b896518b2028d9c4d6ea44806d093fd33793689";
-                sha256 = "sha256-mzNy5GPlB2qkI2KEAErJQzO//uo8yO0kPQUwvGDwR4w=";
-              };
-            } );
-          });
-        })
-      ];
     })
   ];
 }
