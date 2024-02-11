a7365122fc3173022308d4e9aaab367306a07487
Nebula <me@itsnebula.net>
2024-02-11 18:58:48 -0500

    Revert "feat(software): browser stuff"
    This reverts commit f25d4f2c912103728091b1bc6a8c36877a711efb.

--- modules/programs/chromium.nix
+++ modules/programs/chromium.nix
@@ -6,11 +6,7 @@
 }: {
   options.programs.chromium.enable = lib.mkEnableOption "Chromium";
 
-  config.os = lib.mkIf config.programs.chromium.enable {
-    programs.chromium.enable = true;
-    programs.chromium.extensions = [
-      "cjpalhdlnbpafiamejdnhcphjbkeiagm" # uBlock Origin
-      "gejiddohjgogedgjnonbofjigllpkmbf" # 1Password Nightly
-    ];
+  config.hm = lib.mkIf config.programs.chromium.enable {
+    home.packages = [pkgs.chromium];
   };
 }
