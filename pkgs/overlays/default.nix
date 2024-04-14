58ab630439e67765668cb782493b61659fa7f244
Nebula <me@itsnebula.net>
2024-04-14 17:22:55 -0400

    feat(jellyfin): patch menu item into web client

--- pkgs/overlays/default.nix
+++ pkgs/overlays/default.nix
@@ -1,5 +1,6 @@
 _: prev: {
   caddy-custom = prev.callPackage ./caddy-custom.nix {};
+  jellyfin-web = prev.callPackage ./jellyfin-web {};
   nvchad = prev.callPackage ./nvchad.nix {};
   plasma-rose-pine = prev.callPackage ./plasma-rose-pine.nix {};
 }
