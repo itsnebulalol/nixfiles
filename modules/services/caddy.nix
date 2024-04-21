6840662f1a420fbb6fcb0b165d4e8cdab022d363
Nebula <me@itsnebula.net>
2024-04-21 16:15:37 -0400

    fix(services/caddy): import cloudflare

--- modules/services/caddy.nix
+++ modules/services/caddy.nix
@@ -21,18 +21,22 @@
       virtualHosts = {
         "f.itsnebula.net".extraConfig = ''
           reverse_proxy consus.coin-gray.ts.net:7697
+          import cloudflare
         '';
 
         "ig.itsnebula.net".extraConfig = ''
           reverse_proxy consus.coin-gray.ts.net:2858
+          import cloudflare
         '';
 
         "matrix.itsnebula.net".extraConfig = ''
           reverse_proxy /_matrix/* semreh.coin-gray.ts.net:6167
+          import cloudflare
         '';
 
         "nostr.itsnebula.net".extraConfig = ''
           reverse_proxy semreh.coin-gray.ts.net:4629
+          import cloudflare
         '';
       };
     };
